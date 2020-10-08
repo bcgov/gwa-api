f672bf81e05e2d1c5b096b1cdca1a6290d788d8f
ikethecoder <ikethecoder@canzea.com>
2020-10-08 13:43:27 -0700

    Add whoami

---
+++ microservices/gatewayApi/v1/routes/whoami.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+from subprocess import Popen, PIPE
+import uuid
+import logging
+import yaml
+from flask import Blueprint, jsonify, request, Response, make_response, abort, g, current_app as app
+from io import TextIOWrapper
+
+from v1.auth.auth import admin_jwt
+
+whoami = Blueprint('whoami', 'whoami')
+
+@whoami.route('',
+           methods=['GET'], strict_slashes=False)
+@admin_jwt(None)
+def who_am_i() -> object:
+    """
+    :return: JSON of some key information about the authenticated principal
+    """
+    log = app.logger
+
+    if 'team' not in g.principal:
+        abort(make_response(jsonify(error="Missing Claims."), 500))
+
+    output = {
+        "authorized-party": g.principal['azp'],
+        "scope": g.principal['scope'],
+        "issuer": g.principal['iss'],
+        "audience": g.principal['aud'],
+        "namespace": g.principal['team'],
+        "client-address": g.principal['clientAddress'],
+    }
+    return make_response(jsonify(output))
--- microservices/gatewayApi/v1/v1.py
+++ microservices/gatewayApi/v1/v1.py
@@ -2,6 +2,7 @@ import os
 from flask import Blueprint, jsonify
 from functools import reduce
 from v1.routes.gateway import gw
+from v1.routes.whoami import whoami
 from v1.routes.docs import docs
 
 v1 = Blueprint('v1', 'v1')
@@ -18,4 +19,5 @@ class Register:
     def __init__(self, app):
         app.register_blueprint(v1, url_prefix="/v1")
         app.register_blueprint(gw, url_prefix="/v1/gateway")
+        app.register_blueprint(whoami, url_prefix="/v1/whoami")
         app.register_blueprint(docs, url_prefix="/v1/api-docs")
