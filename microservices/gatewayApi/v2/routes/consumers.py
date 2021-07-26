5498186872281f5a9c4364e0e97ffb5871ea3efb
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-07-25 22:55:16 -0700

    formatted the code using autopep8

--- microservices/gatewayApi/v2/routes/consumers.py
+++ microservices/gatewayApi/v2/routes/consumers.py
@@ -7,11 +7,13 @@ from clients.portal import record_custom_event
 from v2.services.gateway_consumers import GatewayConsumerService
 
 consumers = Blueprint('consumers.v2', 'consumers')
+
+
 @consumers.route('/<string:consumer_id>/plugins',
-           methods=['POST'], strict_slashes=False)
+                 methods=['POST'], strict_slashes=False)
 def create_consumer_plugin(namespace: str, consumer_id: str) -> object:
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumerPlugin', 'add','received', namespace)  
+    record_custom_event(event_id, 'GatewayConsumerPlugin', 'add', 'received', namespace)
     log = app.logger
     pluginData = request.get_json()
     cnsr = GatewayConsumerService()
@@ -24,16 +26,18 @@ def create_consumer_plugin(namespace: str, consumer_id: str) -> object:
         try:
             response = cnsr.add_consumer_plugin(consumer_id, pluginData)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin', action='add', namespace=namespace, message='failed to create plugin.')
-        
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin',
+                              action='add', namespace=namespace, message='failed to create plugin.')
+
     record_custom_event(event_id, 'GatewayConsumerPlugin', 'add', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:consumer_id>/plugins/<string:plugin_id>',
-           methods=['PUT'], strict_slashes=False)
+                 methods=['PUT'], strict_slashes=False)
 def update_consumer_plugin(namespace: str, consumer_id: str, plugin_id: str) -> object:
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumerPlugin', 'update', 'received', namespace)   
+    record_custom_event(event_id, 'GatewayConsumerPlugin', 'update', 'received', namespace)
     log = app.logger
     pluginData = request.get_json()
     cnsr = GatewayConsumerService()
@@ -46,13 +50,15 @@ def update_consumer_plugin(namespace: str, consumer_id: str, plugin_id: str) -> 
         try:
             response = cnsr.update_consumer_plugin(consumer_id, plugin_id, pluginData)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin', action='update', namespace=namespace, message='failed to update plugin.')
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin',
+                              action='update', namespace=namespace, message='failed to update plugin.')
 
     record_custom_event(event_id, 'GatewayConsumerPlugin', 'update', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:consumer_id>/plugins/<string:plugin_id>',
-           methods=['DELETE'], strict_slashes=False)
+                 methods=['DELETE'], strict_slashes=False)
 def delete_consumer_plugin(namespace: str, consumer_id: str, plugin_id: str) -> object:
     event_id = str(uuid.uuid4())
     record_custom_event(event_id, 'GatewayConsumerPlugin', 'delete', 'received', namespace)
@@ -61,13 +67,15 @@ def delete_consumer_plugin(namespace: str, consumer_id: str, plugin_id: str) -> 
     try:
         response = cnsr.delete_consumer_plugin(consumer_id, plugin_id)
     except Exception as ex:
-        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin', action='delete', namespace=namespace, message='failed to delete plugin.')
+        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerPlugin',
+                          action='delete', namespace=namespace, message='failed to delete plugin.')
 
     record_custom_event(event_id, 'GatewayConsumerPlugin', 'delete', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:username>',
-           methods=['GET'], strict_slashes=False)
+                 methods=['GET'], strict_slashes=False)
 def get_consumer_by_username(namespace: str, username: str) -> object:
     log = app.logger
     cnsr = GatewayConsumerService()
@@ -77,14 +85,15 @@ def get_consumer_by_username(namespace: str, username: str) -> object:
     except:
         traceback.print_exc()
         log.error("failed to fetch consumer. %s" % sys.exc_info()[0])
-    
+
     return response
 
+
 @consumers.route('',
-           methods=['POST'], strict_slashes=False)
+                 methods=['POST'], strict_slashes=False)
 def create_consumer(namespace: str):
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumer', 'create','received', namespace)    
+    record_custom_event(event_id, 'GatewayConsumer', 'create', 'received', namespace)
     log = app.logger
     consumerData = request.get_json()
     cnsr = GatewayConsumerService()
@@ -97,15 +106,18 @@ def create_consumer(namespace: str):
         try:
             response = cnsr.create_consumer(consumerData)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer', action='create', namespace=namespace, message='failed to create consumer.')
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer',
+                              action='create', namespace=namespace, message='failed to create consumer.')
 
         record_custom_event(event_id, 'GatewayConsumer', 'create', 'completed', namespace)
         return response
+
+
 @consumers.route('/<string:username>',
-           methods=['PUT'], strict_slashes=False)
+                 methods=['PUT'], strict_slashes=False)
 def update_consumer(namespace: str, username: str):
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumer', 'update','received', namespace)
+    record_custom_event(event_id, 'GatewayConsumer', 'update', 'received', namespace)
     log = app.logger
     consumerData = request.get_json()
     cnsr = GatewayConsumerService()
@@ -118,13 +130,15 @@ def update_consumer(namespace: str, username: str):
         try:
             response = cnsr.update_consumer(username, consumerData)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer', action='update', namespace=namespace, message='failed to update consumer.')
-     
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer',
+                              action='update', namespace=namespace, message='failed to update consumer.')
+
     record_custom_event(event_id, 'GatewayConsumer', 'update', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:username>',
-           methods=['DELETE'], strict_slashes=False)
+                 methods=['DELETE'], strict_slashes=False)
 def delete_consumer(namespace: str, username: str) -> object:
     event_id = str(uuid.uuid4())
     record_custom_event(event_id, 'GatewayConsumer', 'delete', 'received', namespace)
@@ -133,16 +147,18 @@ def delete_consumer(namespace: str, username: str) -> object:
     try:
         response = cnsr.delete_consumer(username)
     except Exception as ex:
-        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer', action='delete', namespace=namespace, message='failed to delete consumer.')
+        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumer', action='delete',
+                          namespace=namespace, message='failed to delete consumer.')
 
     record_custom_event(event_id, 'GatewayConsumer', 'delete', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:consumer_id>/key-auth',
-           methods=['POST'], strict_slashes=False)
+                 methods=['POST'], strict_slashes=False)
 def add_consumer_keyauth(namespace: str, consumer_id: str) -> object:
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'add','received', namespace)  
+    record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'add', 'received', namespace)
     log = app.logger
     keyauth_data = request.get_json()
     cnsr = GatewayConsumerService()
@@ -155,16 +171,18 @@ def add_consumer_keyauth(namespace: str, consumer_id: str) -> object:
         try:
             response = cnsr.add_keyauth_to_consumer(consumer_id, keyauth_data)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerKeyAuth', action='add', namespace=namespace, message='failed to add keyauth to consumer.')
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerKeyAuth',
+                              action='add', namespace=namespace, message='failed to add keyauth to consumer.')
 
     record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'add', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:consumer_id>/key-auth/<string:keyauth_id>',
-           methods=['PUT'], strict_slashes=False)
+                 methods=['PUT'], strict_slashes=False)
 def generate_consumer_keyauth_key(namespace: str, consumer_id: str, keyauth_id: str):
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'create','received', namespace)
+    record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'create', 'received', namespace)
     log = app.logger
     key_data = request.get_json()
     cnsr = GatewayConsumerService()
@@ -177,16 +195,17 @@ def generate_consumer_keyauth_key(namespace: str, consumer_id: str, keyauth_id: 
         try:
             response = cnsr.gen_key_for_consumer_keyauth(consumer_id, keyauth_id, key_data)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerKeyAuth', action='update', namespace=namespace, message='failed to generate key for consumer keyauth.')
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerKeyAuth', action='update',
+                              namespace=namespace, message='failed to generate key for consumer keyauth.')
 
     record_custom_event(event_id, 'GatewayConsumerKeyAuth', 'update', 'completed', namespace)
     return response
 
+
 @consumers.route('',
-           methods=['GET'], strict_slashes=False)
+                 methods=['GET'], strict_slashes=False)
 def get_consumers_by_namespace(namespace: str) -> object:
     log = app.logger
-
 
     cnsr = GatewayConsumerService()
     try:
@@ -194,14 +213,14 @@ def get_consumers_by_namespace(namespace: str) -> object:
     except:
         traceback.print_exc()
         log.error("failed to fetch consumers. %s" % sys.exc_info()[0])
-    
+
     return response
 
+
 @consumers.route('/<string:consumer_id>/acls',
-           methods=['GET'], strict_slashes=False)
+                 methods=['GET'], strict_slashes=False)
 def get_consumer_acl_by_namespace(namespace: str, consumer_id: str) -> object:
     log = app.logger
-
 
     cnsr = GatewayConsumerService()
     try:
@@ -209,15 +228,15 @@ def get_consumer_acl_by_namespace(namespace: str, consumer_id: str) -> object:
     except:
         traceback.print_exc()
         log.error("failed to fetch consumer ACL. %s" % sys.exc_info()[0])
-    
+
     return response
 
 
 @consumers.route('/<string:consumer_id>/acls',
-           methods=['POST'], strict_slashes=False)
-def assign_consumer_acl (namespace: str, consumer_id: str) -> object:
+                 methods=['POST'], strict_slashes=False)
+def assign_consumer_acl(namespace: str, consumer_id: str) -> object:
     event_id = str(uuid.uuid4())
-    record_custom_event(event_id, 'GatewayConsumerACL', 'add','received', namespace)
+    record_custom_event(event_id, 'GatewayConsumerACL', 'add', 'received', namespace)
     log = app.logger
     acl_data = request.get_json()
     cnsr = GatewayConsumerService()
@@ -230,13 +249,15 @@ def assign_consumer_acl (namespace: str, consumer_id: str) -> object:
         try:
             response = cnsr.add_consumer_acl(consumer_id, acl_data)
         except Exception as ex:
-            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerACL', action='add', namespace=namespace, message='failed to add consumer ACL.')
+            handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerACL',
+                              action='add', namespace=namespace, message='failed to add consumer ACL.')
 
     record_custom_event(event_id, 'GatewayConsumerACL', 'add', 'completed', namespace)
     return response
 
+
 @consumers.route('/<string:consumer_id>/acls/<string:acl_id>',
-           methods=['DELETE'], strict_slashes=False)
+                 methods=['DELETE'], strict_slashes=False)
 def remove_consumer_acl(namespace: str, consumer_id: str, acl_id: str) -> object:
     event_id = str(uuid.uuid4())
     record_custom_event(event_id, 'GatewayConsumerACL', 'delete', 'received', namespace)
@@ -245,7 +266,8 @@ def remove_consumer_acl(namespace: str, consumer_id: str, acl_id: str) -> object
     try:
         response = cnsr.delete_consumer_acl(consumer_id, acl_id)
     except Exception as ex:
-        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerACL', action='delete', namespace=namespace, message='failed to delete consumer ACL.')
+        handle_exceptions(exception=ex, event_id=event_id, type='GatewayConsumerACL', action='delete',
+                          namespace=namespace, message='failed to delete consumer ACL.')
 
     record_custom_event(event_id, 'GatewayConsumerACL', 'delete', 'completed', namespace)
     return response
@@ -254,11 +276,12 @@ def remove_consumer_acl(namespace: str, consumer_id: str, acl_id: str) -> object
 def handle_exceptions(**kwargs):
     log = app.logger
     if not kwargs['exception'] == None:
-        if  isinstance(kwargs['exception'], HTTPException):
+        if isinstance(kwargs['exception'], HTTPException):
             log.error("%s. %s" % (kwargs['message'], kwargs['exception']))
         else:
             log.error("%s. %s" % (kwargs['message'], sys.exc_info()[0]))
 
         traceback.print_exc()
-        record_custom_event(kwargs['event_id'], kwargs['type'], kwargs['action'], 'failed', kwargs['namespace'], kwargs['message'])
+        record_custom_event(kwargs['event_id'], kwargs['type'], kwargs['action'],
+                            'failed', kwargs['namespace'], kwargs['message'])
         abort(make_response(jsonify(error=kwargs['message']), 400))
--- microservices/gatewayApi/v2/services/gateway_consumers.py
+++ microservices/gatewayApi/v2/services/gateway_consumers.py
@@ -4,6 +4,7 @@ from typing import Any
 from flask import json, make_response, jsonify, abort, current_app as app
 import requests
 
+
 class GatewayConsumerService:
 
     def add_consumer_plugin(self, consumer_id: str, plugin_data: Any):
@@ -22,23 +23,23 @@ class GatewayConsumerService:
 
         return make_http_request("UPDATING CONSUMER PLUGIN", consumer_id, "put", url=url, json=plugin_data, headers={'Content-Type': 'application/json'}, timeout=5)
 
-    def delete_consumer_plugin(self, consumer_id: str, plugin_id: str):    
+    def delete_consumer_plugin(self, consumer_id: str, plugin_id: str):
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + '/plugins/' + plugin_id
         return make_http_request("DELETING CONSUMER PLUGIN", consumer_id, "delete", url=url, headers={'Content-Type': 'application/json'}, timeout=5)
 
-    def get_consumer (self, id_or_name: str):
+    def get_consumer(self, id_or_name: str):
         url = app.config['kongAdminUrl'] + '/consumers/' + id_or_name
         return requests.get(url, timeout=5).json()
 
-    def get_consumers_by_ns (self, namespace: str):
+    def get_consumers_by_ns(self, namespace: str):
         url = app.config['kongAdminUrl'] + '/consumers?tags=ns.' + namespace
         return requests.get(url, timeout=5).json()
 
-    def get_consumer_acl_by_ns (self, consumer_id: str, namespace: str):
+    def get_consumer_acl_by_ns(self, consumer_id: str, namespace: str):
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + '/acls?tags=ns.' + namespace
         return requests.get(url, timeout=5).json()
 
-    def get_consumer_plugin (self, consumer_id: str, plugin_id: str):
+    def get_consumer_plugin(self, consumer_id: str, plugin_id: str):
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + '/plugins/' + plugin_id
         return requests.get(url, timeout=5).json()
 
@@ -61,7 +62,7 @@ class GatewayConsumerService:
     def delete_consumer_acl(self, consumer_id: str, acl_id: str):
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + "/acls/" + acl_id
         return make_http_request("DELETING CONSUMER ACL", consumer_id, "delete", url=url, headers={'Content-Type': 'application/json'}, timeout=5)
-        
+
     def add_keyauth_to_consumer(self, consumer_id: str, keyauth_data: Any):
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + '/key-auth'
         return make_http_request("ADDING KEYAUTH TO CONSUMER", consumer_id, "post", url=url, json=keyauth_data, headers={'Content-Type': 'application/json'}, timeout=5)
@@ -70,6 +71,7 @@ class GatewayConsumerService:
         url = app.config['kongAdminUrl'] + '/consumers/' + consumer_id + '/key-auth/' + keyauth_id
         return make_http_request("GENERATING KEYAUTH KEY", consumer_id, "put", url=url, json=key_data, headers={'Content-Type': 'application/json'}, timeout=5)
 
+
 def response_transformer(response):
     """ 
     Response is update with correct HTTP Error as Kong return 200 for all the requests made
@@ -77,11 +79,11 @@ def response_transformer(response):
     if 'code' in response:
         response['http_error'] = get_http_error_by_kong_code(response['code'])
     return response
-                
+
+
 def get_http_error_by_kong_code(code: int):
     code_dict = {5: 409, 2: 400}
     return code_dict.get(code, 400)
-
 
 
 def add_redis_config(pluginData: str):
@@ -114,4 +116,3 @@ def make_http_request(action: str, id: str, method: str, **rqst_params):
             res = make_response(jsonify(error='failed'), response.status_code)
     log.debug("%s %s[%s]" % (message, action, id))
     return res
-    
