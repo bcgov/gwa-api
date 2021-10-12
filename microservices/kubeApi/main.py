49a0063e01a4893724c36332c53812834570bd82
Nithin Shekar Kuruba <nithinshekar.kuruba@gov.bc.ca>
2021-10-12 12:57:27 -0700

    removed scheduler and updated github actions with additional step to create release version of kube api

--- microservices/kubeApi/main.py
+++ microservices/kubeApi/main.py
@@ -6,8 +6,6 @@ from fastapi.exceptions import HTTPException
 from routers import routes
 from auth.auth import retrieve_token
 from config import settings
-from fastapi_utils.tasks import repeat_every
-from sync.routes import sync_routes
 
 app = FastAPI(title="GWA Kubernetes API",
               description="Description: API to create resources in Openshift using Kubectl",
@@ -36,9 +34,3 @@ def login(request: Request):
 @app.get("/health")
 def check_health():
     return "up"
-
-
-@app.on_event("startup")
-@repeat_every(seconds=int(settings.syncConfig["interval"]))
-def init_jobs():
-    sync_routes()
