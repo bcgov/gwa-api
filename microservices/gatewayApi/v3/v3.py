f54a00f22e5067fd5593adcd192d59fb2a790cb1
ikethecoder <ikethecoder@copeconsulting.ca>
2023-10-04 14:30:37 -0700

    upd v3 of the api to use new deck

--- microservices/gatewayApi/v3/v3.py
+++ microservices/gatewayApi/v3/v3.py
@@ -1,10 +1,7 @@
 from flask import Blueprint, jsonify
-from v2.routes.authz import authz
-from v2.routes.gw_status import gw_status
-from v2.routes.namespaces import ns
-from v2.routes.whoami import whoami
-
 from v3.routes.gateway import gw
+from v3.routes.gw_status import gw_status
+from v3.routes.whoami import whoami
 
 v3 = Blueprint('v3', 'v3')
 
@@ -19,8 +16,6 @@ def get_status():
 class Register:
     def __init__(self, app):
         app.register_blueprint(v3, url_prefix="/v3")
-        app.register_blueprint(authz, url_prefix="/v3/authz")
-        app.register_blueprint(ns, url_prefix="/v3/namespaces")
         app.register_blueprint(gw, url_prefix="/v3/namespaces/<string:namespace>/gateway")
         app.register_blueprint(gw_status, url_prefix="/v3/namespaces/<string:namespace>/services")
         app.register_blueprint(whoami, url_prefix="/v3/whoami")
