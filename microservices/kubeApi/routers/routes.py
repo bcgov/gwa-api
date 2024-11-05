b3d8f2c5c9157e3d3bd7dce74b07881787d9b8a3
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-11-05 09:08:35 -0800

    add debug logging

--- microservices/kubeApi/routers/routes.py
+++ microservices/kubeApi/routers/routes.py
@@ -176,6 +176,8 @@ async def verify_and_create_routes(namespace: str, request: Request):
                 "dataClass": route["metadata"]["annotations"].get("aviinfrasetting.ako.vmware.com/name").split("-")[-1] if route["metadata"]["annotations"].get("aviinfrasetting.ako.vmware.com/name") else None
             }
         )
+    logger.debug("source routes: " + str(source_routes))
+    logger.debug("existing routes: " + str(existing_routes))
 
     insert_batch = [x for x in source_routes if not in_list(x, existing_routes)]
     delete_batch = [y for y in existing_routes if not in_list_by_name(y, source_routes)]
