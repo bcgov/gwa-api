a67f0050d6175481448e0b6d6108e624b82869b9
Russell Vinegar <russell.vinegar@gov.bc.ca>
2024-11-18 09:40:53 -0800

    add kubeapi tests

--- microservices/kubeApi/tests/routers/test_bulk_sync.py
+++ microservices/kubeApi/tests/routers/test_bulk_sync.py
@@ -34,7 +34,8 @@ def test_bulk_sync(client):
                     "host": "abc.api.gov.bc.ca",
                     "sessionCookieEnabled": False,
                     "dataClass": None,
-                    "sslCertificateId": "default"
+                    "sslCertificateId": "default",
+                    "certificates": None
                 }]
                 response = client.post('/namespaces/examplens/routes/sync', json=data)
                 assert response.status_code == 200
@@ -90,4 +91,65 @@ def test_bulk_sync_change_host(client):
                         assert response.json()['message'] == 'synced'
                         assert response.json()['inserted_count'] == 1
                         assert response.json()['deleted_count'] == 1
+
+def test_bulk_sync_change_cert(client):
+    with mock.patch("routers.routes.get_gwa_ocp_routes") as call:
+        call.return_value = [{
+            "metadata": {
+                "name": "wild-ns-example-abc",
+                "labels": {
+                    "aps-select-tag": "ns.EXAMPLE-NS",
+                    "aps-template-version": "v2",
+                    "aps-certificate-id": "41d14845-669f-4dcd-aff2-926fb32a4b25"
+                },
+                "annotations": {
+                }
+            },
+            "spec": {
+                "host": "abc.custom.gov.bc.ca",
+                "to": {
+                    "name": "data-plane-1"
+                }
+            }
+        }]
+
+
+        with mock.patch("routers.routes.prepare_apply_routes") as call_apply:
+            call_apply.return_value = 1
+
+            with mock.patch("routers.routes.apply_routes") as call_mismatch_routes:
+                call_mismatch_routes.return_value = None
+
+                with mock.patch("routers.routes.delete_route") as mock_delete_route:
+                    mock_delete_route.return_value = None  # Simulate successful deletion
+
+                    with mock.patch("routers.routes.kubectl_delete") as mock_kubectl_delete:
+                        mock_kubectl_delete.return_value = None  # Simulate successful kubectl deletion
+
+                        data = [{
+                            "name": "wild-ns-example-abc",
+                            "selectTag": "ns.EXAMPLE-NS",
+                            "dataPlane": "data-plane-1",
+                            "host": "abc.custom.gov.bc.ca",
+                            "sessionCookieEnabled": False,
+                            "dataClass": None,
+                            "sslCertificateId": "default",
+                            "certificates": [                
+                                {
+                                    "id": "afedc6ed-e653-4261-8ec8-17ebaac1fecf",
+                                    "snis": [ "abc.custom.gov.bc.ca" ],
+                                    "cert": "CERT",
+                                    "created_at": 1731713874,
+                                    "tags": [
+                                        "ns.ns1"
+                                    ],
+                                    "key": "KEY",
+                                }              
+                            ] 
+                        }]
+                        response = client.post('/namespaces/examplens/routes/sync', json=data)
+                        assert response.status_code == 200
+                        assert response.json()['message'] == 'synced'
+                        assert response.json()['inserted_count'] == 1
+                        assert response.json()['deleted_count'] == 0
 
--- microservices/kubeApi/tests/routers/test_bulk_sync_new_route.py
+++ microservices/kubeApi/tests/routers/test_bulk_sync_new_route.py
@@ -1,7 +1,7 @@
 from unittest import mock
 import datetime
 
-routes_current_yaml = """
+route_new_yaml = """
 apiVersion: route.openshift.io/v1
 kind: Route
 metadata:
@@ -50,10 +50,10 @@ status:
 def mock_apply_routes (rootPath):
     print(rootPath)
     with open("%s/routes-current.yaml" % rootPath) as f:
-        assert routes_current_yaml == f.read()
+        assert route_new_yaml == f.read()
 
 
-def test_bulk_sync_new_route(client):
+def test_bulk_sync_new_route_custom(client):
 
     with mock.patch('clients.ocp_routes.time_secs') as dt:
         dt.return_value = 1715153983
@@ -84,5 +84,97 @@ def test_bulk_sync_new_route(client):
                         assert response.status_code == 200
                         assert response.json()['message'] == 'synced'
                         assert response.json()['inserted_count'] == 1
+                        
+route_new_custom_yaml = """
+apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: wild-ns-EXAMPLE-NS-abc.custom.gov.bc.ca
+  resourceVersion: ""
+  annotations:
+    haproxy.router.openshift.io/balance: random
+    haproxy.router.openshift.io/disable_cookies: 'true'
+    haproxy.router.openshift.io/timeout: 30m
+
+  labels:
+    aps-generated-by: "gwa-cli"
+    aps-published-on: "2024.05-May.08"
+    aps-namespace: "examplens"
+    aps-select-tag: "ns.EXAMPLE-NS"
+    aps-published-ts: "1715153983"
+    aps-ssl: "tls"
+    aps-data-plane: "data-plane-1"
+    aps-template-version: "v2"
+    aps-certificate-id: "41d14845-669f-4dcd-aff2-926fb32a4b25"
+spec:
+  host: abc.custom.gov.bc.ca
+  port:
+    targetPort: kong-proxy
+  tls:
+    termination: edge
+    insecureEdgeTerminationPolicy: Redirect
+    certificate: |-
+        CERT
+    key: |-
+        KEY
+  to:
+    kind: Service
+    name: data-plane-1
+    weight: 100
+  wildcardPolicy: None
+status:
+  ingress:
+  - host: abc.custom.gov.bc.ca
+    routerName: router
+    wildcardPolicy: None 
+
+---
+"""
+
+def mock_apply_routes_custom (rootPath):
+    print(rootPath)
+    with open("%s/routes-current.yaml" % rootPath) as f:
+        assert route_new_custom_yaml == f.read()
+
+
+def test_bulk_sync_new_route(client):
+
+    with mock.patch('clients.ocp_routes.time_secs') as dt:
+        dt.return_value = 1715153983
+
+        with mock.patch("routers.routes.get_gwa_ocp_routes") as call:
+            call.return_value = []
+
+            with mock.patch("clients.ocp_routes.prepare_route_last_version") as call_last_ver:
+                call_last_ver.return_value = []
+
+                with mock.patch("routers.routes.apply_routes") as call_mismatch_routes:
+                    call_mismatch_routes.side_effect = mock_apply_routes_custom
+
+                    data = [{
+                        "name": "wild-ns-example",
+                        "selectTag": "ns.EXAMPLE-NS",
+                        "dataPlane": "data-plane-1",
+                        "host": "abc.custom.gov.bc.ca",
+                        "sessionCookieEnabled": False,
+                        "dataClass": None,
+                        "sslCertificateId": "default",
+                        "certificates": [                
+                            {
+                                "id": "41d14845-669f-4dcd-aff2-926fb32a4b25",
+                                "snis": [ "abc.custom.gov.bc.ca" ],
+                                "cert": "CERT",
+                                "created_at": 1731713874,
+                                "tags": [
+                                    "ns.ns1"
+                                ],
+                                "key": "KEY",
+                            }              
+                        ]
+                    }]
+                    response = client.post('/namespaces/examplens/routes/sync', json=data)
+                    assert response.status_code == 200
+                    assert response.json()['message'] == 'synced'
+                    assert response.json()['inserted_count'] == 1
 
 
