4753b03fe482ca077838e7b2c4a785f53d999e56
Russell Vinegar <russell.vinegar@gov.bc.ca>
2025-02-18 11:05:58 -0800

    make .env optional

--- microservices/compatibilityApi/config/settings.py
+++ microservices/compatibilityApi/config/settings.py
@@ -1,6 +1,7 @@
 from starlette.config import Config
+import os
 
 # Config will be read from environment variables and/or ".env" files.
-config = Config(".env")
+config = Config(env_file=".env" if os.path.exists(".env") else None)
 
 log_level = config('LOG_LEVEL', default='DEBUG') 
