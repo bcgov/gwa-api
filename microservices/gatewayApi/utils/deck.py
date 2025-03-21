45820fee637438ed26fe4d79cd96ecbd5903610c
ikethecoder <ikethecoder@copeconsulting.ca>
2025-03-21 14:35:20 -0700

    upd deck cli in dockerfile

--- microservices/gatewayApi/utils/deck.py
+++ microservices/gatewayApi/utils/deck.py
@@ -1,12 +1,12 @@
 
 def deck_cmd_sync_diff(deck_cli, cmd, select_tag, state):
-    if deck_cli.startswith("deck_kong2_"):
-        return [ deck_cli, cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag, "--state", state]
-    else:
+    if deck_cli == "deck" or deck_cli.startswith("deck_kong3_"):
         return [ deck_cli, "gateway", cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag, state]
+    else:
+        return [ deck_cli, cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag, "--state", state]
 
 def deck_cmd_validate(deck_cli, state):
-    if deck_cli.startswith("deck_kong2_"):
-        return [ deck_cli, "validate", "--config", "/tmp/deck.yaml", "--state", state ]
-    else:
+    if deck_cli == "deck" or deck_cli.startswith("deck_kong3_"):
         return [ deck_cli, "gateway", "validate", "--config", "/tmp/deck.yaml", state ]
+    else:
+        return [ deck_cli, "validate", "--config", "/tmp/deck.yaml", "--state", state ]
