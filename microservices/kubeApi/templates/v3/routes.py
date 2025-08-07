2fbef9faed44472e2c99894630359267f6278834
ikethecoder <ikethecoder@copeconsulting.ca>
2025-08-06 21:13:03 -0700

    add v3 template for passthrough

---
+++ microservices/kubeApi/templates/v3/routes.py
@@ -0,0 +1,42 @@
+from string import Template
+
+V3_ROUTE = Template("""
+apiVersion: route.openshift.io/v1
+kind: Route
+metadata:
+  name: ${name}
+  resourceVersion: "${resource_version}"
+  annotations:
+    haproxy.router.openshift.io/balance: random
+    haproxy.router.openshift.io/disable_cookies: 'true'
+    haproxy.router.openshift.io/timeout: 30m
+${data_class_annotation}
+  labels:
+    aps-generated-by: "gwa-cli"
+    aps-published-on: "${fmt_time}"
+    aps-namespace: "${ns}"
+    aps-select-tag: "${select_tag}"
+    aps-published-ts: "${timestamp}"
+    aps-ssl: "${ssl_ref}"
+    aps-data-plane: "${data_plane}"
+    aps-template-version: "${template_version}"
+${custom_cert_label}
+spec:
+  host: ${host}
+  port:
+    targetPort: kong-proxy-tls
+  tls:
+    termination: passthrough
+    insecureEdgeTerminationPolicy: None
+  to:
+    kind: Service
+    name: ${service_name}
+    weight: 100
+  wildcardPolicy: None
+status:
+  ingress:
+  - host: ${host}
+    routerName: router
+    wildcardPolicy: None 
+""")
+
