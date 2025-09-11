85391cc51654ed099dba6cb74ad37a6b2540e09a
ikethecoder <ikethecoder@copeconsulting.ca>
2025-09-10 17:46:00 -0700

    add dump support

--- microservices/gatewayApi/utils/deck.py
+++ microservices/gatewayApi/utils/deck.py
@@ -2,6 +2,8 @@
 def deck_cmd_sync_diff(deck_cli, cmd, select_tag, state):
     if deck_cli == "deck" or deck_cli.startswith("deck_kong3_"):
         return [ deck_cli, "gateway", cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag, state]
+    elif deck_cli == "deck" and cmd == "dump":
+        return [ deck_cli, "gateway", cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag]
     else:
         return [ deck_cli, cmd, "--config", "/tmp/deck.yaml", "--skip-consumers", "--select-tag", select_tag, "--state", state]
 
